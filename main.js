fe882b8c46bc5460c78ab1cc3cd4f31a3d9f730b
MedhoMs <danielbucaloiu@gmail.com>
2025-11-12 20:51:44 +0000

    Try to fix dialog iframe load 3

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 const dialog = document.getElementById("dialog-content");
 const fileFolder = document.querySelectorAll(".file-folder"); //Todas las imagenes con esa clase
 let closeDialog = document.getElementById("close-dialog");
-let iframeDialog = document.getElementById("iframe-dialog");
-const windowTitle = document.getElementById("window-title");
+let iframeDialog = document.getElementsByClassName("iframe-dialog"); //Todos los iframes
+const windowTitle = document.getElementById("window-title"); //Div con el titulo de la ventana y el boton de cerrar
 ///////DISPLAYS///////
 
 let dynamicTitle = document.getElementById("dynamic-title"); //El h2 del dialog
 
 ///////DISPLAYS///////
 
+//Objeto que uso para las rutas del iframe
 const pages = {
     "Proyectos" : "projects",
     "Contacto" : "contact",
     "Lenguajes.txt" : "languages",
 }
 
-const pagesDictKeys = Object.keys(pages);
-const pagesDictValues = Object.values(pages);
-const pagesDict = Object.entries(pages);
+const pagesDictKeys = Object.keys(pages); //Obtengo solo las keys
+const pagesDict = Object.entries(pages); //Obtengo las keys y values
 
 let elementSelected; //Usado para el e.currentTarget
 let title; //Usado para extraer el innerHTML del siguiente elemento del e.currentTarget, el <p>nombre</p> del file/folder
 let assignPages; //Usado para extraer el valor del objeto "pages", e introducirlo en el "src" del <iframe>, optimizacion pura
+let showIframeDialog; //Usado para seleccionar el iframe de los que hay en el dialog y decidir si mostrarlo o no;
 
 for (let i = 0; i < fileFolder.length; i++) {
     fileFolder[i].addEventListener("mouseup", openFileFolder);
@@ -51,20 +52,27 @@ dialog.addEventListener("mouseup", () => {
 //////LOGICA MOVER DIALOG//////
 
 function openFileFolder(e) {
-    elementSelected = e.currentTarget;
-    title = elementSelected.nextElementSibling.innerHTML;
-    dynamicTitle.innerHTML = title;
+    elementSelected = e.currentTarget; //Selecciono el elemento que acabo de pulsar (la imagen)
+    title = elementSelected.nextElementSibling.innerHTML; //El "title" sera el contenido del siguiente elemento de elementSelected(imagen), que sera un <p>
+    dynamicTitle.innerHTML = title; //Contenido del h2 del dialog
 
-    if (pagesDictKeys.includes(title)) {
+    if (pagesDictKeys.includes(title)) { //Si en las keys del objeto se encuentra "title":
 
         for (let i = 0; i < pagesDictKeys.length; i++) {
-            if (title === pagesDictKeys[i]) {
-                assignPages = pagesDict[i][1];
+            if (title === pagesDictKeys[i]) { //Si "title" es igual a alguna de las keys:
+                assignPages = pagesDict[i][1]; //A "assignPages" le asigno el value correspondiente a la key[i]
             }
         }
     }
     
-    iframeDialog.setAttribute("src", `/pages/${assignPages}.html`);
+    for (let i = 0; i < iframeDialog.length; i++) {
+        iframeDialog[i].hidden = true; //Pongo todos los iframes en hidden cuando llamo a openFileFolder()
+    }
+
+    showIframeDialog = document.getElementById(`iframe-${assignPages}`); //Selecciono el iframe especifico segun su id
+    showIframeDialog.hidden = false; //Le quito el hidden para que solo se muestre ese
+    
+    //iframeDialog.setAttribute("src", `/pages/${assignPages}.html`);
 
     dialog.showModal();
 }
