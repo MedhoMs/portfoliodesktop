92c4afffdb55065af8888c07d371e2f0ae71de6a
MedhoMs <danielbucaloiu@gmail.com>
2025-11-13 01:46:14 +0000

    Added footer, date-time, change background and display options

--- main.js
+++ main.js
@@ -3,11 +3,22 @@ const fileFolder = document.querySelectorAll(".file-folder"); //Todas las imagen
 let closeDialog = document.getElementById("close-dialog");
 let iframeDialog = document.getElementsByClassName("iframe-dialog"); //Todos los iframes
 const windowTitle = document.getElementById("window-title"); //Div con el titulo de la ventana y el boton de cerrar
+let footerTime = document.getElementById("footer-time"); //Para mostrar el tiempo
+let footerDate = document.getElementById("footer-date"); //Para mostrar la fecha
+let windowsButton = document.getElementById("windows-button") //La imagen de windows del footer
+let leftArrow = document.getElementById("left-arrow");
+let rigthArrow = document.getElementById("rigth-arrow");
+let desktop = document.getElementById("desktop");
+
 ///////DISPLAYS///////
 
 let dynamicTitle = document.getElementById("dynamic-title"); //El h2 del dialog
+let windowsButtonOptions = document.getElementById("windows-button-options"); //Es una ventana de options que se abre al pulsar la imagen de windows
 
 ///////DISPLAYS///////
+
+
+///////OBJECTS-LISTS///////
 
 //Objeto que uso para las rutas del iframe
 const pages = {
@@ -19,16 +30,57 @@ const pages = {
 const pagesDictKeys = Object.keys(pages); //Obtengo solo las keys
 const pagesDict = Object.entries(pages); //Obtengo las keys y values
 
+//Esta funcion hace que tanto el mes como el dia tengan un 0 delante cuando solo son 1 cifra
+function pad(num) {
+    return num.toString().padStart(2, '0');
+}
+
+function updateTimeDate() {
+    const timeDate = new Date();
+
+    const year = timeDate.getFullYear();
+    const month = pad(timeDate.getMonth() + 1);
+    const day = pad(timeDate.getDate());
+    const hour = pad(timeDate.getHours());
+    const minute = pad(timeDate.getMinutes());
+    const second = pad(timeDate.getSeconds());
+
+    footerTime.innerHTML = `${hour}:${minute}:${second}`;
+    footerDate.innerHTML = `${day}/${month}/${year}`;
+}
+
+setInterval(updateTimeDate, 1000);
+updateTimeDate();
+
+
+const backgroundImagesList = ["/imgs/background-desktop0.jpg", "/imgs/background-desktop1.jpg", "/imgs/background-desktop2.jpg", "/imgs/background-desktop3.jpg"];
+
+console.log(backgroundImagesList[0]);
+console.log(backgroundImagesList[1]);
+
+///////OBJECTS-LISTS///////
+
 let elementSelected; //Usado para el e.currentTarget
 let title; //Usado para extraer el innerHTML del siguiente elemento del e.currentTarget, el <p>nombre</p> del file/folder
 let assignPages; //Usado para extraer el valor del objeto "pages", e introducirlo en el "src" del <iframe>, optimizacion pura
 let showIframeDialog; //Usado para seleccionar el iframe de los que hay en el dialog y decidir si mostrarlo o no;
+let windowsOptionActive = true; //Usado para determinar si las opciones de windows estan abiertas o cerradas
+let backgroundIndexList = 0; //Usado para identificar en que posicion de la lista de backgrounds estoy
+
+///////EVENTS///////
 
 for (let i = 0; i < fileFolder.length; i++) {
     fileFolder[i].addEventListener("mouseup", openFileFolder);
 }
 
 closeDialog.addEventListener("mouseup", closeDialogAction);
+windowsButton.addEventListener("mouseup", showWindowsOptions);
+leftArrow.addEventListener("mouseup", changeBackDesktopBackward);
+rigthArrow.addEventListener("mouseup", changeBackDesktopForward);
+
+///////EVENTS///////
+
+
 
 //////LOGICA MOVER DIALOG//////
 
@@ -50,6 +102,9 @@ dialog.addEventListener("mouseup", () => {
 });
 
 //////LOGICA MOVER DIALOG//////
+
+
+//////FUNCTIONS//////
 
 function openFileFolder(e) {
     elementSelected = e.currentTarget; //Selecciono el elemento que acabo de pulsar (la imagen)
@@ -82,3 +137,67 @@ function closeDialogAction() {
     dialog.style.left = "30%";
     dialog.requestClose();
 }
+
+
+function showWindowsOptions() {
+
+    if (windowsOptionActive == true) {
+        windowsButtonOptions.style.opacity = "1";
+    } else {
+        windowsButtonOptions.style.opacity = "0";
+    }
+
+    windowsOptionActive = !windowsOptionActive;
+}
+
+window.onload = () => {
+    checkArrowsEvents(); //Para desactivar de primeras la flecha de la izquierda
+};
+
+function changeBackgroundImage() {
+    //desktop.style.opacity = 0;
+    //setTimeout(() => {
+        desktop.style.backgroundImage = `url('${backgroundImagesList[backgroundIndexList]}')`;
+        //desktop.style.opacity = 1;
+    //}, 400);
+    
+    checkArrowsEvents();
+}
+
+function changeBackDesktopBackward() {
+
+    if (backgroundIndexList != 0) {
+        backgroundIndexList -= 1;
+    }
+
+    changeBackgroundImage();
+}
+
+function changeBackDesktopForward() {
+
+    if (backgroundIndexList != backgroundImagesList.length - 1) {
+        backgroundIndexList += 1;
+    }
+
+    changeBackgroundImage();
+}
+
+function checkArrowsEvents() {
+
+    if (backgroundIndexList === 0) {
+        leftArrow.style.pointerEvents = "none";
+    } else {
+        leftArrow.style.pointerEvents = "all";
+    }
+        
+    if (backgroundIndexList === (backgroundImagesList.length - 1)) {
+        rigthArrow.style.pointerEvents = "none";
+    } else {
+        rigthArrow.style.pointerEvents = "all";
+    }
+}
+//////FUNCTIONS//////
+
+
+//Hacer que backgroundImagesList sea un objeto con la imagen (key) y el color de las letras (value), para que cuando el
+//fondo sea oscuro, las letras se cambien a white, y viceversa
